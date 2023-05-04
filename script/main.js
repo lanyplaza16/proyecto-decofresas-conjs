2c3085b2f029c3e7dd06aa6db717580ad8400d93
Lany Plaza <lanyplaza@gmail.com>
2023-05-03 23:18:32 -0300

    Idea Plaza

--- script/main.js
+++ script/main.js
@@ -1,66 +1,83 @@
 // Aca escribo mi JS
 
-// Crear la clase combo
-class Combo {
-  constructor(numero, nombre, precio) {
-    this.numero = numero;
-    this.nombre = nombre;
-    this.precio = precio;
-  }
-}
+// Comienzo de JS para Productos
 
-// Crear los combos con sus atributos
+//Cree los combos en formato json
+
 const combos = [
-  new Combo(1, "Box London", 4700),
-  new Combo(2, "Box Grecia", 8800),
-  new Combo(3, "Box Venecia", 13700)
+  {
+    "nombre": "Box London",
+    "descripcion": "8 frutillas decoradas a tu gusto acompañado por 1 botella de Chamdon 187ml, 12 Ferrero Rocher y flores de temporada.",
+    "precio": 4700
+  },
+  {
+    "nombre": "Box Grecia",
+    "descripcion": "8 frutillas decoradas a tu gusto acompañado por 1 botella de Chamdon 187ml y 1 chocolate.",
+    "precio": 8800
+  },
+  {
+    "nombre": "Box Venecia",
+    "descripcion": "8 frutillas decoradas a tu gusto acompañado por 6 rosas y 12 ferrero rocher.",
+    "precio": 13700
+  }
 ];
 
-// Funcion para calcular el total de la combra con las variables nombre del combo y cantidad aca utilizo el metodo find para conseguir el combo seleccionado
-function calcularTotal(combo, cantidad) {
-  let nombreCombo;
-  let precioCombo;
+// Modificación del DOM
 
-  const comboSeleccionado = combos.find(c => c.nombre.toLowerCase() === combo.toLowerCase());
-  if (comboSeleccionado) {
-    nombreCombo = comboSeleccionado.nombre;
-    precioCombo = comboSeleccionado.precio;
-  } else {
-    return "";
+const seccionCombo = document.getElementById("seccionCombo");
+
+const btnSeleccionar1 = document.getElementById("botonLondon");
+btnSeleccionar1.addEventListener("click", function() {
+  actualizarSeccionCombo(0);
+});
+
+const btnSeleccionar2 = document.getElementById("botonGrecia");
+btnSeleccionar2.addEventListener("click", function() {
+  actualizarSeccionCombo(1);
+});
+
+const btnSeleccionar3 = document.getElementById("botonVenecia");
+btnSeleccionar3.addEventListener("click", function() {
+  actualizarSeccionCombo(2);
+});
+
+function actualizarSeccionCombo(indiceCombo) {
+  const combo = combos[indiceCombo];
+  seccionCombo.innerHTML = `
+    <h2>${combo.nombre}</h2>
+    <p>${combo.descripcion}</p>
+    <p>Precio: $${combo.precio}</p>
+  `;
+}
+
+// Fin de JS para Productos
+
+// Comienzo de JS para Contactos
+
+//Guardar datos
+
+function guardarDatos() {
+  const nombre = document.getElementById("exampleFormControlInput1").value;
+  const telefono = document.getElementById("exampleFormControlInput2").value;
+  const email = document.getElementById("exampleFormControlInput3").value;
+
+  localStorage.setItem("exampleFormControlInput1", nombre);
+  localStorage.setItem("exampleFormControlInput2", telefono);
+  localStorage.setItem("exampleFormControlInput3", email);
+
+  alert("Datos guardados correctamente");
+}
+
+window.onload = function() {
+  const nombreGuardado = localStorage.getItem("exampleFormControlInput1");
+  const telefonoGuardado = localStorage.getItem("exampleFormControlInput2");
+  const emailGuardado = localStorage.getItem("exampleFormControlInput3");
+
+  if (nombreGuardado && telefonoGuardado && emailGuardado) {
+    alert(`Bienvenido ${nombreGuardado}, su teléfono es ${telefonoGuardado} y su email es ${emailGuardado}`);
   }
-
-  return nombreCombo + " - $" + precioCombo * cantidad;
 }
+// Fin de JS para Contactos
 
-// Simulador de compra al solicitar al usuario ingresar un nombre de combo
-alert("¡Gracias por elegirnos!");
-
-let comboValido = false;
-let combo;
-
-while (!comboValido) {
-  combo = prompt("Por favor, indica el nombre del combo que quieres escoger: Box London, Box Grecia o Box Venecia");
-
-  if (combos.some(c => c.nombre.toLowerCase() === combo.toLowerCase())) {
-    comboValido = true;
-  } else {
-    alert("El nombre del combo ingresado no existe. Por favor, intente con un nombre de combo descrito en pantalla.");
-  }
-}
-
-console.log("El combo escogido es " + combo);
-
-//Solicitar al usuario la cantidad a comprar
-let cantidad = parseInt(prompt("Por favor, ingrese la cantidad que desea comprar"));
-
-console.log("Ha ingresado el combo " + combo + " y desea comprar " + cantidad + " unidades.");
-
-let totalPagar = calcularTotal(combo, cantidad);
-
-if (totalPagar !== "") {
-  alert("El precio total de su compra es " + totalPagar);
-} else {
-  alert("Ha ocurrido un error al calcular el precio total. Por favor, intente de nuevo.");
-}
 
 
